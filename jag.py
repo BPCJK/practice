6ffbb18519fb038c9bff949d13ee4b6b92e16f96
BPCJK <46236028+BPCJK@users.noreply.github.com>
2020-04-08 01:03:36 -0700

    commit trials

--- jag.py
+++ jag.py
@@ -3,7 +3,7 @@ import os
 
 
 def printMama(num):
-    print("MSou " + str(num))
+    print("Mama " + str(num))
     val = num / 2.0
     return val
 
@@ -14,9 +14,6 @@ printMama(3)
 for i in range(1, 10):
     printMama(i)
 
-for j in range(10):
-    print(10)
-
 mamaList = [
     "Mama",
     "Jag",
@@ -25,3 +22,6 @@ mamaList = [
     "Jag4",
     "Jag2",
 ]
+
+for j in range(2, 10):
+    printMama(i)
